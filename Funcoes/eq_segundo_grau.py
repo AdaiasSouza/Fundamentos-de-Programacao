ec5c0d66a637c3824374a3ae576b2163985bbd83
AdaiasSouza <adaias_305@hotmail.com>
2024-02-11 20:07:23 -0300

    Encapsulando a equação do segundo grau em uma função

--- Funcoes/eq_segundo_grau.py
+++ Funcoes/eq_segundo_grau.py
@@ -13,23 +13,35 @@ def delta_eq(a,b,c):
         delta_negat = (mt.pow(b,2) - (4*a*c))
         return delta_negat
 
-
-
-print("\tEq Segundo Grau\n")
-
-# Coeficientes de teste
-#a,b,c = 3,-7,4
-#a,b,c = 9,-12,4
-#a,b,c = 5,3,5
-a,b,c = 2,1,-3
-
-if delta_eq(a,b,c) >= 0:
+def equacao_grau2(a, b, c):
+    delta_eq(a, b, c)
     x_0 = (-b + delta_eq(a,b,c))/(2*a)
     x_1 = (-b - delta_eq(a,b,c))/(2*a)
 
+    return x_0, x_1
+
+
+def main():
+    print("\tEq Segundo Grau\n")
+
+    # Coeficientes de teste
+    #a,b,c = 3,-7,4
+    #a,b,c = 9,-12,4
+    #a,b,c = 5,3,5
+    a,b,c = 2,1,-3
+
+    # if delta_eq(a,b,c) >= 0:
+    #     x_0 = (-b + delta_eq(a,b,c))/(2*a)
+    #     x_1 = (-b - delta_eq(a,b,c))/(2*a)
+
+    #     print("x_0 = %.2f"%x_0,"x_1 = %.2f"%x_1)
+
+    #else: print("Raiz nao real:  ",delta_eq(a,b,c))
+    x_0, x_1 = equacao_grau2(a, b, c)
     print("x_0 = %.2f"%x_0,"x_1 = %.2f"%x_1)
 
-else: print("Raiz nao real:  ",delta_eq(a,b,c))
+if __name__ == '__main__':
+    main()
 
 
 
